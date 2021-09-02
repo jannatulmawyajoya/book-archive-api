3949a72e15fba24668067dec7a3bdd73d3072dcd
jannatulmawyajoya <jannatuljoya228@gmail.com>
2021-09-02 10:46:48 +0600

    all commited

--- book.js
+++ book.js
@@ -4,7 +4,6 @@ const loadData = () => {
     const inputFieldText = inputField.value ;
     // clearing the input field
     inputField.value = '';
-    
     // generated the url 
     const url =`http://openlibrary.org/search.json?q=${inputFieldText}`;
     fetch(url)
@@ -12,29 +11,45 @@ const loadData = () => {
         .then(data => displayData(data.docs));
 };
 
+const displayError = () => {
+    document.getElementById('error-msg').innerText = 'No result found';
+    document.getElementById('book-numbers').style.display = 'none';
+}
+
 const displayData = books =>{
-    const first20books =books.slice(0,30);
+    
+    const first30books =books.slice(0,30);
     // show total search result 
-    document.getElementById('book-numbers').innerText= `Total Books: ${first20books.length}`;
+    document.getElementById('book-numbers').innerText= `Total Books: ${first30books.length}`;
     const searchContainer = document.getElementById('search-container');
     searchContainer.textContent ='';
-   
-    first20books.forEach(book =>{
-        //    console.log(book);
-        const bookImage = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML =`
+
+    if (first30books.indexOf(first30books.length)=== -1){
+        displayError();
+        
+    }
+        first30books.forEach(book => {
+            document.getElementById('book-numbers').style.display = 'block';
+            document.getElementById('error-msg').innerText = '';
+            console.log(book);
+            const bookImage = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
             <div class="card">
-            <img src="${bookImage ? bookImage: 'N/a'}" class="card-img-top" width="200px" height="400px" alt="...">
+            <img src="${bookImage ? bookImage : 'No image found'}" class="card-img-top" width="200px" height="400px" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Title: ${book.title ? book.title: 'N/a'}</h5>
-                <h5 class="card-title">Author Name: ${ book.author_name ? book.author_name[0]: 'N/a'}</h5>
-                <h5 class="card-title">Publish Year: ${book.first_publish_year ? book.first_publish_year: 'N/a'}</h5>
+                <h5 class="card-title">Title: ${book.title ? book.title : 'N/a'}</h5>
+                <h5 class="card-title">Author Name: ${book.author_name ? book.author_name[0] : 'N/a'}</h5>
+                <h5 class="card-title">Publish Year: ${book.first_publish_year ? book.first_publish_year : 'N/a'}</h5>
+                <h5 class="card-title">Publisher Name: ${book.publisher ? book.publisher[0] : 'No publisher found'}</h5>
             </div>
         </div>
             
         `;
-        searchContainer.appendChild(div);
-    })
-}
+            searchContainer.appendChild(div);
+        })
+    
+    };
+
+       
